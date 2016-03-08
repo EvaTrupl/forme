b701e0a697dfe3d998e83d3cf34d14ad647dcc62
Ruža Ilinović <ruza.ilinovic@skole.hr>
2016-03-08 17:33:42 +0100

    Procistioli smo PHP.

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Naslov</title>
+    <meta charset=""UTF-8">
+    <title>Forme</title>
 </head>
-
 <body>
-<h1>Prvi</h1>
-
-<p>
-    <?php
-    echo("Pozdrav iz PHP-a");
-    echo("<br>");
-    echo(date("d.m.Y."));
-    ?>
-</p>
-
-<p>
-    <?php
-    $dana_do_praznika = 45;
-    echo("Do praznika je ostalo jos:<b>" . $dana_do_praznika . "</b>");
-    ?>
-</p>
+<h1>Forme</h1>
 
 </body>
 </html>
